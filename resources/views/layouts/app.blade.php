a37ca041e766fbef71414e446b82f30c27811a8b
abdhilal <abdrahmanmhran3@gmail.com>
2025-11-24 21:57:57 +0300

    refactor(views): clean up header and layout templates
    - Remove unused cart, notification, and bookmark dropdowns from header
    - Simplify user profile section to use auth data
    - Fix formatting and whitespace in layout files
    - Adjust sidebar positioning and event border styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html >
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}" dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}">
 @include('layouts.partials.head')
-  <body class="{{ app()->getLocale() === 'ar' ? 'rtl' : '' }}">
+
+<body class="{{ app()->getLocale() === 'ar' ? 'rtl' : '' }}">
     @include('layouts.partials.alert')
     <!-- page-wrapper Start-->
     <!-- tap on top starts-->
@@ -10,18 +11,19 @@
     @include('layouts.partials.loader')
     <div class="page-wrapper compact-wrapper" id="pageWrapper">
         @include('layouts.partials.header')
-      <!-- Page Body Start-->
-      <div class="page-body-wrapper">
-        <!-- Page sidebar start-->
-      @auth
-          @include('layouts.partials.aside')
-      @endauth
-        <!-- Page sidebar end-->
+        <!-- Page Body Start-->
+        <div class="page-body-wrapper">
+            <!-- Page sidebar start-->
+            @auth
+                @include('layouts.partials.aside')
+            @endauth
+            <!-- Page sidebar end-->
 
-        @include('layouts.partials.body')
-        @include('layouts.partials.footer')
-      </div>
+            @include('layouts.partials.body')
+            @include('layouts.partials.footer')
+        </div>
     </div>
     @include('layouts.partials.scripts')
-  </body>
+</body>
+
 </html>
